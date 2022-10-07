17ea15ad20b4e3b0a46996336ff03b04d646d343
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-10-07 13:28:32 +0900

    read_map

--- srcs/main.c
+++ srcs/main.c
@@ -6,6 +6,6 @@ int	main(int ac, char **av)
 
 	check_arguments(ac, av);
 	init_all(&game);
-	read_map(av[1]); //유효성 검사 및 파싱
+	read_map(av[1], game); //유효성 검사 및 파싱
 	print_map();
 }
--- srcs/read_map.c
+++ srcs/read_map.c
@@ -2,24 +2,52 @@
 #include "../libft/libft.h"
 #include <fcntl.h>
 
-void	read_wall_texture(char *line)
+void	compare_and_save(char **splited, t_cube_info *cube_info)
 {
-	char	**splited_line;
-
-	ft_split(line, );
+	if (ft_strncmp(splited[0], "NO", 3))
+		cube_info->NO = splited[1];
+	else if (ft_strncmp(splited[0], "SO", 3))
+		cube_info->SO = splited[1];
+	else if (ft_strncmp(splited[0], "WE", 3))
+		cube_info->WE = splited[1];
+	else if (ft_strncmp(splited[0], "EA", 3))
+		cube_info->EA = splited[1];
+	else if (ft_strncmp(splited[0], "F", 2))
+		cube_info->F = splited[1];	
+	else if (ft_strncmp(splited[0], "C", 2))
+		cube_info->C = splited[1];
+	else
+		print_error_and_exit("wrong information\n");
+	free(splited[0]);
 }
 
-void	read_map(char *name_of_map)
+void	read_wall_texture(char *line, t_game *game)
+{
+	static char	spaces[7] = {'\t', '\n', '\v', '\f', '\r', ' ', 0};
+	char		**splited;
+
+	if (ft_word_count(line, spaces) == 0)
+		return ;
+	if (ft_word_count(line, spaces) != 2)
+		print_error_and_exit("wrong information\n");
+	splited = ft_split(line, spaces);
+	compare_and_save(splited, game->cube_info);
+	free(splited);
+}
+
+void	read_map(char *name_of_map, t_game *game)
 {
 	char	*line;
 	int		fd;
 
-	fd = open(name_of_map, O_RDONLY);
+	fd = open(name_of_map, O_RDONLY);// open 해서 유효성 검사
 	line = get_next_line(fd);
 	while (line)
 	{
-		read_wall_texture(line);
+		read_wall_texture(line, game);
 		free(line);
 		line = get_next_line(fd);
 	}
+	fd = open(name_of_map, O_RDONLY);// malloc 해서 파싱
+	
 }
